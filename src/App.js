e4c14ea1972c700711ba7ee86ad8c37fdbee490d
sajidsaeed85 <51917585+sajidsaeed85@users.noreply.github.com>
2022-06-16 06:48:02 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
-import React from "react";
-import { View } from "react-native";
-import { Canvas } from "react-three-fiber";
-import { OrbitControls } from "@react-three/drei";
-import { Physics } from "use-cannon";
-import styles from "./styles";
-// 床
-import Floor from "./components/Floor";
-// サボテンたち
-import { EnemyData } from "./components/Enemy";
+import React, { useEffect } from "react";
+import { StyleSheet, View, Animated } from "react-native";
 
-/*
- * 表示される入り口
- */
-const App = () => {
+export default function App() {
+  const animated = new Animated.Value(0);
+  const duration = 5000;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animated, {
+          toValue: 400,
+          duration: duration,
+          useNativeDriver: true
+        }),
+        Animated.timing(animated, {
+          toValue: 0,
+          duration: duration,
+          useNativeDriver: true
+        })
+      ])
+    ).start();
+  });
   return (
-    <View style={styles.app}>
-      <Canvas
-        shadowMap
-        camera={{
-          position: [0, 2, 10],
-          near: 0.1,
-          far: 50,
-        }}
-      >
-        <ambientLight intensity={0.5} />
-        <pointLight position={[-20, 30, 30]} intensity={0.4} castShadow />
-        <OrbitControls />
-        <Physics
-          gravity={[0, -30, 0]}
-          defaultContactMaterial={{ restitution: 0 }}
-        >
-          {/* 横に移動するサボテンたち */}
-          <EnemyData number={10} />
-          {/* 床 */}
-          <Floor position={[0, -1, 0]} args={[100, 0.5, 100]} />
-        </Physics>
-      </Canvas>
+    <View style={styles.container}>
+      <Animated.View style={[{ transform: [{ translateY: animated }] }]}>
+        <View style={styles.circle} />
+      </Animated.View>
+      <Animated.View style={[{ transform: [{ translateY: animated }] }]}>
+        <View style={styles.circle2} />
+      </Animated.View>
+      <Animated.View style={[{ transform: [{ translateY: animated }] }]}>
+        <View style={styles.circle3} />
+      </Animated.View>
     </View>
   );
-};
-export default App;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#000",
+    alignItems: "baseline",
+    justifyContent: "center"
+  },
+  circle: {
+    width: 160,
+    height: 160,
+    borderRadius: 160 / 2,
+    backgroundColor: "#d9d8d6",
+    margin: 40
+  },
+  circle2: {
+    width: 160,
+    height: 160,
+    borderRadius: 160 / 2,
+    backgroundColor: "#53b3d3",
+    margin: 40
+  },
+  circle3: {
+    width: 160,
+    height: 160,
+    borderRadius: 160 / 2,
+    backgroundColor: "#3a82aa",
+    margin: 40
+  }
+});
